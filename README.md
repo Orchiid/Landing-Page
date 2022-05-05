69a0df8f5ce0e9cef7caca3142b8f530d6c2b421
Orchiid <joannatolofari@gmail.com>
2022-05-05 23:00:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Landing Page
+
+This is a landing page of a portfolio. Made for Udacity Landing Page Project.
+
+# Table of contents
+
+- [Project Title](#project-title)
+- [Table of contents](#table-of-contents)
+- [Installation](#installation)
+- [License](#license)
+- [Footer](#footer)
+
+# Installation
+[(Back to top)](#table-of-contents)
+
+
+To use this project, first clone the repo on your device using the command below:
+
+```git init```
+
+```git clone https://github.com/Orchiid/landingpage.git``` -->
+
+
+
+# License
+[(Back to top)](#table-of-contents)
+
+[GNU General Public License version 3](https://opensource.org/licenses/GPL-3.0)
+
+# Footer
+[(Back to top)](#table-of-contents)
+
+Leave a star in GitHub, give a clap in Medium and share this guide if you found this helpful.
---
+++ js/app.js
@@ -0,0 +1,97 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+// sections global variable
+const parts = Array.from(document.querySelectorAll('section'));
+// nav global variables
+const navBar = document.getElementById('navbar__list');
+let listItemsNo = parts.length
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+function createList() {
+    for (section of parts) {
+        partName = section.getAttribute('data-nav');
+        partLink = section.getAttribute('id');
+        // create each list item
+        listItem = document.createElement('li');
+        listItem.innerHTML = `<a class='menu__link' href='#${partLink}'> ${partName} </a>`
+
+        // Add listItem to navBar
+        navBar.appendChild(listItem);
+    }
+}
+
+function viewportPart (sec) {
+    let partPosition = sec.getBoundingClientRect()
+    return (partPosition.top >= 0);
+}
+
+// Changes background of section viewed
+
+function toggleActive(){
+    for (section of parts) {
+        if (viewportPart(section)) {
+            if (!section.classList.contains('your-active-class')) {
+                section.classList.add('your-active-class')
+            }
+        } else {
+            section.classList.remove('your-active-class')
+        }
+    }
+}
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+createList();
+
+// Add class 'active' to section when near top of viewport
+
+document.addEventListener('scroll', toggleActive);
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
